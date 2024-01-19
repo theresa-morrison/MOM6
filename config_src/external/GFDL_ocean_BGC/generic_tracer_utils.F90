e014089314352f3c07e42a8a101f2863a1dbf702
Kate Hedstrom <kshedstrom@alaska.edu>
2024-01-19 03:43:47 -0900

    Bringing config_src updates from esmg (#2)
    - otherwise it doesn't compile without linking to COBALT due to
       COBALT interface updates.

--- config_src/external/GFDL_ocean_BGC/generic_tracer_utils.F90
+++ config_src/external/GFDL_ocean_BGC/generic_tracer_utils.F90
@@ -108,7 +108,8 @@ contains
     type(g_diag_ctrl),  target,intent(in) :: diag_CS !< Unknown
   end subroutine g_tracer_set_csdiag
 
-  subroutine g_tracer_set_common(isc,iec,jsc,jec,isd,ied,jsd,jed,nk,ntau,axes,grid_tmask,grid_kmt,init_time)
+  subroutine g_tracer_set_common(isc,iec,jsc,jec,isd,ied,jsd,jed,nk,ntau,axes,grid_tmask,grid_kmt,init_time, &
+                                 geolon,geolat)
     integer,                     intent(in) :: isc !< Computation start index in i direction
     integer,                     intent(in) :: iec !< Computation end index in i direction
     integer,                     intent(in) :: jsc !< Computation start index in j direction
@@ -123,6 +124,8 @@ contains
     real, dimension(isd:,jsd:,:),intent(in) :: grid_tmask !< Unknown
     integer,dimension(isd:,jsd:),intent(in) :: grid_kmt !< Unknown
     type(time_type),             intent(in) :: init_time !< Unknown
+    real, dimension(isd:,jsd:),  intent(in) :: geolon !< Longitude
+    real, dimension(isd:,jsd:),  intent(in) :: geolat !< Latitude
   end subroutine g_tracer_set_common
 
   subroutine g_tracer_get_common(isc,iec,jsc,jec,isd,ied,jsd,jed,nk,ntau,&
